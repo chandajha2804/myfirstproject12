b4ab913adadf04026cce63c752620147c562a618
Chanda <chandajha2804@gmail.com>
2026-07-13 15:39:38 +1000

    my update

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-shopping_list = []
+# Take three inputs from the user
+print("Enter the first item")
+item1 = input()
+
+print("Enter the second item")
+item2 = input()
+
+print("Enter the third item")
+item3 = input()
+
+# Create the shopping_list containing only those three items
+shopping_list = [item1, item2, item3]
+
+# Print the shopping_list data structure
+print(shopping_list)
